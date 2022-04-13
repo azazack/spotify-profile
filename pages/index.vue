c4448138b340b0f82e991ac843c2f42c0dc67b9f
azazack <zakaria.azf@pre-history.com>
2022-04-13 23:00:56 +0000

    chore: first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .test tes
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  name: 'Index',
+})
+export default class Index extends Vue {}
+</script>
